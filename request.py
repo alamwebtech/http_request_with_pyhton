dbb7443d19185165800aacd7a8949f51cbb9f30f
Md Alam <alamwebtech@gmail.com>
2019-02-03 12:13:08 -0500

    getting json data in python

--- request.py
+++ request.py
@@ -2,6 +2,6 @@ import requests
 url = "https://icanhazdadjoke.com/"
 
 
-response = requests.get(url, headers={"Accept":"text/plain"})
+response = requests.get(url, headers={"Accept":"application/jason"})
 print(response.text)
 print(response.json())
